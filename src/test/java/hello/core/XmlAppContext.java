14d220f40820d0b822154557da49a543720b7221
BanseokSuh <still3028@gmail.com>
2023-07-17 00:09:43 +0900

    빈 설정 메타정보 확인 테스트 코드 작성

--- src/test/java/hello/core/XmlAppContext.java
+++ src/test/java/hello/core/XmlAppContext.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
