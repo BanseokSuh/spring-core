2e901078cc62e4aa4fe532a02074dcc81fa124b2
BanseokSuh <still3028@gmail.com>
2023-07-23 00:15:01 +0900

    Spring @Configuration 바이트 코드 조작 관련 테스트

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+//    @Autowired
+//    MemberRepository memberRepository;
+
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
+//        return new MemberServiceImpl(memberRepository);
     }
 
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
+//        return new OrderServiceImpl(memberRepository, discountPolicy());
     }
 
     @Bean
