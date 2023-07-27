7fb51e8157ac0ea5d52733df942f364fbcc253dc
BanseokSuh <still3028@gmail.com>
2023-07-28 00:22:56 +0900

    컴포넌트 스캔 시 basePackages 관련 테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core", //컴포넌트 스캔의 대상이 되는 패키지 설정 가능, 복수 패키지 설정 가능 -> basePackages = {"hello.core", "hello.service"}
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+
+// basePackages 지정하지 않으면 -> @Configuration이 붙은 설정 정보 클래스 파일이 있는 경로의 모든 파일 탐색 : hello.core
 public class AutoAppConfig {
 }
