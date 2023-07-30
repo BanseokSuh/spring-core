792658ac7c13948d92a0a1b73a3e203f06076f48
BanseokSuh <still3028@gmail.com>
2023-07-30 19:23:33 +0900

    중복 등록 및 충돌 관련

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,10 @@ import org.springframework.context.annotation.FilterType;
 
 // basePackages 지정하지 않으면 -> @Configuration이 붙은 설정 정보 클래스 파일이 있는 경로의 모든 파일 탐색 : hello.core
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
