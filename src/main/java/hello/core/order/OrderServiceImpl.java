821226d6e12363db625c296b4229d88220cede4c
BanseokSuh <still3028@gmail.com>
2023-07-31 01:15:45 +0900

    필드 주입 관련 코드 작성 및 주석 처리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,25 +9,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    //필드 주입은 외부에서 필드 변경이 불가능해서 테스트하기 어려움.
+    //스프링 설정을 목적으로 하는 @Configuration에서만 특별하게 사용하자.
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy; //구체적인 Impl을 의존하지 않음
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; //구체적인 Impl을 의존하지 않음
 
-//    @Autowired //수정자 주입은 빈 등록 후 다음 단계에서 일어남
+    //수정자 주입은 빈 등록 후 다음 단계에서 일어남
+//    @Autowired
 //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
 //        System.out.println("discountPolicy = " + discountPolicy);
 //        this.discountPolicy = discountPolicy;
 //    }
-//
 //    @Autowired
 //    public void setMemberRepository(MemberRepository memberRepository) {
 //        System.out.println("memberRepository = " + memberRepository);
 //        this.memberRepository = memberRepository;
 //    }
 
-    @Autowired //생성자가 하나일 때는 자동으로 Autowired가 주입됨. 생성자 주입은 빈을 등록하면서 생성자 주입이 같이 일어남.
+    //생성자 주입은 빈을 등록과 의존성 주입이 같이 일어남.
+    //생성자가 하나일 때는 자동으로 Autowired가 주입됨.
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
 
