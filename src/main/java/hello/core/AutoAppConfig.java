41b64d76b42d4af5f9660359d4a42ae70d59151c
BanseokSuh <still3028@gmail.com>
2023-08-06 22:00:23 +0900

    전체 테스트 시 이슈 빈 충돌 이슈 수정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -20,9 +20,8 @@ import org.springframework.context.annotation.FilterType;
 // basePackages 지정하지 않으면 -> @Configuration이 붙은 설정 정보 클래스 파일이 있는 경로의 모든 파일 탐색 : hello.core
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
