f18d41ef135b18fe8d8288901bf9056c65169305
BanseokSuh <still3028@gmail.com>
2023-08-21 21:41:32 +0900

    함수명 오타 수정

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 public class BeanLifeCycleTest {
     @Test
-    public void lefeCycleTest() {
+    public void lifeCycleTest() {
         ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
         ac.close();
