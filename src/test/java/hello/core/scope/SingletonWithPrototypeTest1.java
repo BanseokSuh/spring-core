459535fa223e48b82194be6483aa09bfbdb22cdf
BanseokSuh <still3028@gmail.com>
2023-08-26 16:06:31 +0900

    ObjectProvider - 지정한 빈을 찾아주는 DL 서비스를 제공

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,8 @@
 package hello.core.scope;
 
+import hello.core.annotation.MainDiscountPolicy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -36,40 +38,45 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입. x01
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입. x01
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
 //        @Autowired
-//        ApplicationContext applicationContext;
+//        ApplicationContext ac;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
-//            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
 
-    @Scope("singleton")
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입. x02
-
-        public ClientBean2(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic() {
-            prototypeBean.addCount();
-            return prototypeBean.getCount();
-        }
-    }
+//    @Scope("singleton")
+//    static class ClientBean2 {
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입. x02
+//
+//        public ClientBean2(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic() {
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+//    }
 
     @Scope("prototype")
     static class PrototypeBean {
